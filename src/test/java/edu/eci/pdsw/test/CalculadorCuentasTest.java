6e6b731d2838a863cb95f98942fb2b9ebc683edc
Alejandra Gomez <alejita.9813.dg@gmail.com>
2017-09-05 09:31:14 -0500

    implementación pruebas

--- src/test/java/edu/eci/pdsw/test/CalculadorCuentasTest.java
+++ src/test/java/edu/eci/pdsw/test/CalculadorCuentasTest.java
@@ -5,6 +5,9 @@
  */
 package edu.eci.pdsw.test;
 
+import edu.eci.pdsw.examples.beans.*;
+import edu.eci.pdsw.examples.beans.impl.*;
+import edu.eci.pdsw.examples.model.*;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -35,6 +38,36 @@ public class CalculadorCuentasTest {
     
     @Before
     public void setUp() {
+    }
+    
+    @Test
+    public void calculadorBasicoCuentasTest() {
+        Orden or = new Orden();
+        Bebida be = new Bebida("Pepsi", 2000, 100);
+        or.agregarItemOrden(be);
+        CalculadorBasicoCuentas cbc = new CalculadorBasicoCuentas();
+        assertEquals(cbc.calcularCosto(or), 2000);
+        
+    }
+    
+    @Test
+    public void calculadorCuentaConPropinaTest() {
+        Orden or = new Orden();
+        Bebida be = new Bebida("Pepsi", 2000, 1001);
+        or.agregarItemOrden(be);
+        CalculadorCuentaConPropina cccp = new  CalculadorCuentaConPropina();
+        assertEquals(cccp.calcularCosto(or), 2580);
+        
+    }
+    
+    @Test
+    public void calculadorCuentaConIvaTest() {
+        Orden or = new Orden();
+        Plato pl = new Plato("Bandejapaisa", 2000);
+        or.agregarItemOrden(pl);
+        CalcularCuentaConIva cci = new  CalcularCuentaConIva();
+        assertEquals(cci.calcularCosto(or), 2380);
+        
     }
     
 }
